91de4f398d0904b72ecd93c0212ee18080aa9346
raftaar1191 <raftaar1191@gmail.com>
2023-04-26 18:43:56 +0530

    Add plugin

---
+++ includes/class-wordpress-plugin-boilerplate-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://acrosswp.com
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Plugin_Boilerplate
+ * @subpackage Wordpress_Plugin_Boilerplate/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Plugin_Boilerplate
+ * @subpackage Wordpress_Plugin_Boilerplate/includes
+ * @author     AcrossWP <contact@acrosswp.com>
+ */
+class Wordpress_Plugin_Boilerplate_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-wordpress-plugin-boilerplate-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://acrosswp.com
+ * @since      1.0.0
+ *
+ * @package    Wordpress_Plugin_Boilerplate
+ * @subpackage Wordpress_Plugin_Boilerplate/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Wordpress_Plugin_Boilerplate
+ * @subpackage Wordpress_Plugin_Boilerplate/includes
+ * @author     AcrossWP <contact@acrosswp.com>
+ */
+class Wordpress_Plugin_Boilerplate_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'wordpress-plugin-boilerplate',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
