8667f617d66a37dc7c3c102b160c0a1fc42f6ccf
raftaar1191 <raftaar1191@gmail.com>
2023-10-18 21:09:33 +0530

    Update plugin

--- admin/licenses-update/plugin-update-checker/main.php
+++ admin/licenses-update/plugin-update-checker/main.php
@@ -1,1 +1,19 @@
 <?php
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Add Github Plugin update checker into the AcrossWP Github Plugin Update Checker
+ */
+function wordpress_plugin_boilerplate_plugins_update_checker_github( $packages ) {
+
+    $packages[1000] = array(
+        'repo' 		        => 'https://github.com/acrosswp/acrosswp-testing-plugin-update',
+        'file_path' 		=> WORDPRESS_PLUGIN_BOILERPLATE_FILES,
+        'plugin_name_slug'	=> WORDPRESS_PLUGIN_BOILERPLATE_PLUGIN_NAME_SLUG,
+        'release_branch' 	=> 'main'
+    );
+
+    return $packages;
+}
+add_filter( 'acrosswp_plugins_update_checker_github', 'wordpress_plugin_boilerplate_plugins_update_checker_github', 100, 1 );
