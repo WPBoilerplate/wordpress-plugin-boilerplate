9617af57d2a427589f1bc5880ab4cec1798bf474
raftaar1191 <raftaar1191@gmail.com>
2024-05-15 12:34:11 +0530

    Update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'acrosswp/wordpress-plugin-boilerplate',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0f3527005ee66f8ce537b1738b42b0c8634496a3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'acrosswp/acrosswp-about-us' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce0279d30e0963b892bd18202af2a72e318c9993',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../acrosswp/acrosswp-about-us',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'acrosswp/wordpress-plugin-boilerplate' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0f3527005ee66f8ce537b1738b42b0c8634496a3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
