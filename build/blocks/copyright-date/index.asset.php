4b0a8b0a27ed9d8722bf6b4a11fc9d15ae10e99d
raftaar1191 <raftaar1191@gmail.com>
2024-05-26 22:05:49 +0530

    Remove unwanted code

--- build/blocks/copyright-date/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1157879e8e41944d0d93');
--- build/blocks/todo-list/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'cd58090b20934c3e24bc');
