d65505ba63c20e2e3f946e73fc15041d40e3744f
raftaar1191 <raftaar1191@gmail.com>
2024-07-25 22:12:43 +0530

    Add code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpboilerplate/wordpress-plugin-boilerplate',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '42fde605ed53ae3bb481b5645b6c957766c34cb5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'szepeviktor/package-path' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '18f93ac05953340216f76e5fef0b9e1c664e7f70',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../szepeviktor/package-path',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'wpboilerplate/wordpress-plugin-boilerplate' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '42fde605ed53ae3bb481b5645b6c957766c34cb5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpboilerplate/wpb-about-us' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6f1138791caa9470da4320ee8b21dd453b25e403',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpboilerplate/wpb-about-us',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'wpboilerplate/wpb-plugins-info' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9595d31d8b225c512bf3d2bc7eada320c5581cc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpboilerplate/wpb-plugins-info',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ vendor/wpboilerplate/wpb-plugins-info/index.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * BuddyBoss Compatibility Integration Class.
+ *
+ * @since BuddyBoss 1.1.5
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Check if the class does not exits then only allow the file to add
+ */
+if( ! class_exists( 'WPBoilerplate_Plugins_Info' ) ) {
+	/**
+	 * Fired during plugin licences.
+	 *
+	 * This class defines all code necessary to run during the plugin's licences and update.
+	 *
+	 * @since      0.0.1
+	 * @package    WPBoilerplate_Plugins_Info
+	 * @subpackage WPBoilerplate_Plugins_Info/includes
+	 * @author     WPBoilerplate <contact@acrosswp.com>
+	 */
+	class WPBoilerplate_Plugins_Info {
+
+		/**
+		 * The single instance of the class.
+		 *
+		 * @var WPBoilerplate_Plugins_Info
+		 * @since 0.0.1
+		 */
+		protected static $_instance = null;
+
+		/**
+		 * Main WPBoilerplate_Plugins_Info Instance.
+		 *
+		 * Ensures only one instance of WooCommerce is loaded or can be loaded.
+		 *
+		 * @since 0.0.1
+		 * @static
+		 * @see WPBoilerplate_Plugins_Info()
+		 * @return WPBoilerplate_Plugins_Info - Main instance.
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+
+		/**
+		 * Get the vendor path of composer
+		 * 
+		 * @return string Path of the vendor dir
+		 */
+		public function get_vendor_path() {
+			return \SzepeViktor\Composer\PackagePath::getVendorPath();	
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		public function get_plugin_path() {
+			return dirname( $this->get_vendor_path() );
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		public function get_full_plugin_path() {
+			return $this->get_plugin_path() . '/' . $this->get_plugin_file_name() . '.php';
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		public function get_plugin_file_name() {
+			return basename( $this->get_plugin_path() );
+		}
+
+		/**
+		 * Get the plugin path
+		 * 
+		 * @return string Path of the plugins
+		 */
+		public function get_block_path() {
+			return $this->get_plugin_path() . '/build';
+		}
+	}
+
+	WPBoilerplate_Plugins_Info::instance();
+}
