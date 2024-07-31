fd73189feb6e54c4e0cbbb76a1a69f34fc4a16b7
raftaar1191 <raftaar1191@gmail.com>
2024-08-01 01:41:12 +0530

    Update code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpboilerplate/wordpress-plugin-boilerplate',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '812d97775e0826afbbf6b7aa2a6be1d97ea7f88d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpboilerplate/wordpress-plugin-boilerplate' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '812d97775e0826afbbf6b7aa2a6be1d97ea7f88d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
